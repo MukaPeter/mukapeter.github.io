d04aec1996a7e499eeb7293a111d8057af0cacd7
MukaPeter <muka.peti@gmail.com>
2019-02-23 13:27:46 +0100

    postok mentése

--- blog.js
+++ blog.js
@@ -1,5 +1,21 @@
 $('#button_upload').click((event) => {
     var title_input = $('#title_input').val();
     var content_input= $('#content_input').val();
-    console.log (title_input, content_input)
-    })
+    console.log (title_input, content_input);
+
+    $("ul").append(`  <li>
+    <h3>${title_input}</h3>
+    <p>${content_input}</p>
+    </li>`);
+
+
+    let path = "posts/1";
+    let dataToSave = {
+        title: title_input,
+        text: content_input
+    };
+
+    fb.ref(path).set(dataToSave);
+
+});
+
