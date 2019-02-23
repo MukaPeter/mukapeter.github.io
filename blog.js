7ac5bb301b61f9a55bfec9d2673a7bbe036f02cb
MukaPeter <muka.peti@gmail.com>
2019-02-23 12:41:44 +0100

    JS modositas

---
+++ blog.js
@@ -0,0 +1,5 @@
+$('#button_upload').click((event) => {
+    var title_input = $('#title_input').val();
+    var content_input= $('#content_input').val();
+    console.log (title_input, content_input)
+    })
